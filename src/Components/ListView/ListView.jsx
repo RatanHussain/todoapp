7ed7976c6d5a0c6ff07ec673311ccce0358f658a
Ratan Hussain <mdratanmia7395@gmail.com>
2024-07-08 23:24:58 +0300

    updated

--- src/Components/ListView/ListView.jsx
+++ src/Components/ListView/ListView.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 
 export default function ListView() {
+	console.log((8/2(2+2)))
 	return (
 		<div>
 			<h1>List view</h1>
--- src/Components/todos/MainTodo.jsx
+++ src/Components/todos/MainTodo.jsx
@@ -8,14 +8,14 @@ export default function MainTodo() {
 	return (
 		<div>
 			<h1>index file</h1>
-			<ListView/>
+			<ListView todo={ todo } />
 		</div>
 	);
 }
 
 let todo = {
-	text: 'alarm',
 	id: '234524',
+	text: 'alarm',
 	desctiption: '3 to 4',
 	time: '34:43',
 	isComplete: false,
